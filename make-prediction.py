fc68d741dbf8a76481097da1bcedec457ad90baf
T-Visor <turkimbro@yahoo.com>
2021-06-28 14:46:39 -0400

    added comments to make-prediction.py code

--- make-prediction.py
+++ make-prediction.py
@@ -16,19 +16,30 @@ MAX_PIXEL_VALUE = 255.0 # 8-bit color depth with range 0-255
 
 
 def run_example():
+    """
+        Demonstrate the prediction capability of the trained convolutional
+        neural network.
+    """
     model = load_model('my_model/')
-
-    image_tensor = load_image_as_tensor('sample_image.png')
-
-    digit = model.predict_classes(image_tensor)
-
-    display_classification_results('sample_image.png', digit)
+    image_tensor = load_image_as_compatible_array('sample_image.png')
+    prediction = model.predict_classes(image_tensor)
+    display_classification_results('sample_image.png', prediction)
 
 
 
 
-def load_image_as_tensor(filename):
-    image = load_img(filename, grayscale=True, target_size=(IMAGE_LENGTH, IMAGE_WIDTH))
+def load_image_as_compatible_array(file_name):
+    """
+        Convert the input image to a 3D array for compatibility with the
+        convolutional neural network. 
+    
+    Args:
+        file_name (string): name of the image file
+
+    Returns:
+        a 3D Numpy array representing the image
+    """
+    image = load_img(file_name, grayscale=True, target_size=(IMAGE_LENGTH, IMAGE_WIDTH))
     image = img_to_array(image)
     image = image.reshape(1, IMAGE_LENGTH, IMAGE_WIDTH, COLOR_CHANNELS)
 
@@ -41,6 +52,13 @@ def load_image_as_tensor(filename):
 
 
 def display_classification_results(file_name, prediction):
+    """
+        Display the image and its classification result.
+
+    Args:
+        file_name (string): name of the image file
+        prediction (string): the result from the convolutional neural network
+    """
     prediction = str(prediction).strip('[]')
     formatted_prediction = 'Prediction: ' + prediction
 
